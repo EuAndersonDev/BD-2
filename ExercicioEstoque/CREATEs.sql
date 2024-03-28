b09378c9c149cb15a850da9558dbd6b10e50154a
Anderson Reis <137122362+AndersonReis04@users.noreply.github.com>
2024-03-28 12:43:27 -0300

    Add files via upload
    Só as tabelas

---
+++ ExercicioEstoque/CREATEs.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE bdEstoque
+GO
+
+USE bdEstoque
+
+CREATE TABLE tbCliente (
+	codCliente INT PRIMARY KEY IDENTITY(1,1)
+	,nomeCliente VARCHAR(100) NOT NULL
+	,cpfCliente VARCHAR(11) NOT NULL
+	,emailCliente VARCHAR(150) NOT NULL
+	,sexoCliente CHAR NOT NULL
+	,dataNascimentoCliente DATE NOT NULL
+);
+
+CREATE TABLE tbVenda (
+	codVenda INT PRIMARY KEY IDENTITY(1,1)
+	,dataVenda DATE NOT NULL
+	,valorTotalVenda FLOAT NOT NULL
+	,codCliente INT FOREIGN KEY REFERENCES tbCliente(codCliente)
+);
+
+CREATE TABLE tbFabricante(
+	codFabricante INT PRIMARY KEY IDENTITY(1,1)
+	,nomeFabricante VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tbFornecedor(
+	codFornecedor INT PRIMARY KEY IDENTITY(1,1)
+	,nomeFornecedor VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tbProduto(
+	codProduto INT PRIMARY KEY IDENTITY(1,1)
+	,descricaoProduto VARCHAR(100) NOT NULL
+	,valorProduto FLOAT NOT NULL
+	,quantidadeProduto INT NOT NULL
+	,codFabricante INT FOREIGN KEY REFERENCES tbFabricante(codFabricante)
+	,codFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(codFornecedor)
+);
+
+CREATE TABLE tbItensVenda(
+	codItensVenda INT PRIMARY KEY IDENTITY(1,1)
+	,codVenda INT FOREIGN KEY REFERENCES tbVenda(codVenda)
+	,codProduto INT FOREIGN KEY REFERENCES tbProduto(codProduto)
+	,quantidadeItensVenda INT NOT NULL
+	,subTotalItensVenda INT NOT NULL
+);
