26704ad879bbb68a376f78392156dbd8bc7ee7ad
AndersonReis04 <andersonaugustorei1227@gmail.com>
2024-04-03 19:13:30 -0300

    Resolvido

--- ExercicioEstoque/CREATEs.sql
+++ ExercicioEstoque/CREATEs.sql
@@ -1,48 +1,53 @@
-CREATE DATABASE dbEstoque
+CREATE DATABASE	dbEstoque
 GO
 
 USE dbEstoque
 
 CREATE TABLE tbCliente (
-	codCliente INT PRIMARY KEY IDENTITY(1,1)
-	,nomeCliente VARCHAR(100) NOT NULL
-	,cpfCliente VARCHAR(11) NOT NULL
-	,emailCliente VARCHAR(150) NOT NULL
-	,sexoCliente CHAR NOT NULL
-	,dataNascimentoCliente DATE NOT NULL
+	codCliente INT IDENTITY (1,1) PRIMARY KEY
+	,nomeCliente VARCHAR (100)
+	,cpfCliente VARCHAR (11)
+	,emailCliente VARCHAR (50)
+	,sexoCliente CHAR (1)
+	,dataNascimentoCliente DATE
 );
 
 CREATE TABLE tbVenda (
-	codVenda INT PRIMARY KEY IDENTITY(1,1)
+	codVenda INT IDENTITY (1,1) PRIMARY KEY
 	,dataVenda DATE NOT NULL
 	,valorTotalVenda FLOAT NOT NULL
-	,codCliente INT FOREIGN KEY REFERENCES tbCliente(codCliente)
+	,codCliente INT NOT NULL
+	,FOREIGN KEY (codCliente) REFERENCES tbCliente (codCliente)
+);
+ 
+CREATE TABLE tbFabricante (
+	codFabricante INT IDENTITY (1,1) PRIMARY KEY
+	,nomeFabricante VARCHAR (50) NOT NULL
 );
 
-CREATE TABLE tbFabricante(
-	codFabricante INT PRIMARY KEY IDENTITY(1,1)
-	,nomeFabricante VARCHAR(100) NOT NULL
+CREATE TABLE tbFornecedor (
+	codFornecedor INT IDENTITY (1,1) PRIMARY KEY
+	,nomeFornecedor VARCHAR (50) NOT NULL
+	,contatoFornecedor VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tbFornecedor(
-	codFornecedor INT PRIMARY KEY IDENTITY(1,1)
-	,nomeFornecedor VARCHAR(100) NOT NULL
-	,contatoFornecedor VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE tbProduto(
-	codProduto INT PRIMARY KEY IDENTITY(1,1)
-	,descricaoProduto VARCHAR(100) NOT NULL
+CREATE TABLE tbProduto (
+	codProduto INT IDENTITY (1,1) PRIMARY KEY
+	,descricaoProduto VARCHAR (100) NOT NULL
 	,valorProduto FLOAT NOT NULL
 	,quantidadeProduto INT NOT NULL
-	,codFabricante INT FOREIGN KEY REFERENCES tbFabricante(codFabricante)
-	,codFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(codFornecedor)
+	,codFabricante INT NOT NULL
+	,codFornecedor INT NOT NULL
+	,FOREIGN KEY (codFabricante) REFERENCES tbFabricante (codFabricante)
+	,FOREIGN KEY (codFornecedor) REFERENCES tbFornecedor (codFornecedor)
 );
 
-CREATE TABLE tbItensVenda(
-	codItensVenda INT PRIMARY KEY IDENTITY(1,1)
-	,codVenda INT FOREIGN KEY REFERENCES tbVenda(codVenda)
-	,codProduto INT FOREIGN KEY REFERENCES tbProduto(codProduto)
+CREATE TABLE tbItensVenda (
+	codItensVenda INT IDENTITY (1,1) PRIMARY KEY
+	,codVenda INT NOT NULL
+	,codProduto INT NOT NULL
 	,quantidadeItensVenda INT NOT NULL
-	,subTotalItensVenda INT NOT NULL
+	,subTotalItensVenda FLOAT NOT NULL
+	,FOREIGN KEY (codVenda) REFERENCES tbVenda (codVenda)
+	,FOREIGN KEY (codProduto) REFERENCES tbProduto (codProduto)
 );
--- ExercicioEstoque/INSERTs.sql
+++ ExercicioEstoque/INSERTs.sql
@@ -1,53 +1,53 @@
-USE dbEstoque;
+USE dbEstoque
 GO
 
 INSERT INTO tbCliente(nomeCliente, cpfCliente, emailCliente, sexoCliente, dataNascimentoCliente) 
-VALUES 
-    ('Armando Jose Santana', '12345678900', 'armandojsantana@outlook.com', 'M', '1961-02-21')
-    ,('Shela Carvalho Leal', '45678909823', 'acarvalho@ig.com.br', 'F', '1978-09-13')
-    ,('Carlos Henrique Souza', '76598278299', 'chenrique@ig.com.br', 'M', '1981-09-08')
-    ,('Maria Aparecida Souza', '87365309899', 'mapdasouza@outlook.com', 'F', '1962-07-07')
-    ,('Adriana Nogueira Silva', '76354309388', 'drica1977@ig.com.br', 'F', '1977-04-09')
-    ,('Paulo Henrique Silva', '87390123111', 'phaiva80@hotmail.com', 'M', '1987-02-02');
+VALUES
+	('Amanda José Santana', '12345678900', 'amandojsantana@outlook.com', 'M', '1961-02-21')
+	,('Sheila Carvalho Real', '45678909823', 'scarvalho@ig.com.br', 'F', '1978-09-13')
+	,('Carlos Henrique Souza', '76598278299', 'chenrique@ig.com.br', 'M', '1981-09-08')
+	,('Maria Aparecida Souza', '87365309899', 'mapdasouza@outlook.com', 'F', '1962-07-07')
+	,('Adriana Nogueira Silva', '76354309388', 'drica1977@ig.com.br', 'F', '1977-04-09')
+	,('Paulo Henrique Silva', '87390123111', 'phsilva@hotmail.com', 'M', '1987-02-02');
 
-INSERT INTO tbFabricante (nomeFabricante) 
-VALUES 
-	('Uniever')
+INSERT INTO tbFabricante(nomeFabricante) 
+VALUES
+	('Unilever')
 	,('P&G')
 	,('Bunge');
 
-INSERT INTO tbFornecedor (nomeFornecedor, contatoFornecedor) 
-VALUES 
+INSERT INTO tbFornecedor(nomeFornecedor, contatoFornecedor) 
+VALUES
 	('Atacadão', 'Carlos Santos')
-	,('Asaa', 'Maria Stella')
-	,('Roldão', 'Paulo Céear');
+	,('Assai', 'Maria Stella')
+	,('Roldão', 'Paulo César');
 
-INSERT INTO tbProduto (descricaoProduto, valorProduto, quantidadeProduto, codFabricante, codFornecedor) 
-VALUES 
-	('Amaciante Downy', 5.76, 1500, 2,1)
-	,('Amaciante Confort', 4.45, 2300,1,1)
-	,('Sabão em pó OMO', 5.99, 1500, 1, 2)
-	,('Margaina Qualy', 4.76, 2500, 3,1)
+INSERT INTO tbProduto(descricaoProduto, valorProduto, quantidadeProduto, codFabricante, codFornecedor) 
+VALUES
+	('Amaciante Downy', 5.56, 1500, 2, 1)
+	,('Amaciante Comfort', 5.45, 2300, 1, 1)
+	,('Sabão em pó OMO', 5.99, 1280, 1, 2)
+	,('Margarina Qually', 4.76, 2500, 3, 1)
 	,('Salsicha Hot Dog Sadia', 6.78, 2900, 3, 2)
 	,('Mortadela Perdigão', 2.50, 1200, 3, 3)
-	,('Hamburguer Sadia', 9.89, 1600, 3,1)
+	,('Hamburguer Sadia', 9.98, 1600, 3, 1)
 	,('Fralda Pampers', 36.00, 340, 2, 3)
-	,('Xampu Seda', 5.89, 800, 1,2)
+	,('Xampu Seda', 5.89, 800, 1, 1)
 	,('Condicionador Seda', 6.50, 700, 1, 3);
 
-INSERT INTO tbVenda (dataVenda, valorTotalVenda, codCliente) 
-VALUES 
-	('01-02-2014', 4500.00, 1)
-	,('03-02-2014', 3400, 1)
-	,('10-02-2014', 2100, 2)
-	,('15-02-2014', 2700, 3)
-	,('17-03-2014', 560, 3)
-	,('09-04-2014', 1200, 4)
-	,('07-05-2014', 3500, 5)
-	,('07-05-2014', 3400, 1)
-	,('05-05-2014', 4000, 2);
+INSERT INTO tbVenda(codCliente, dataVenda, valorTotalVenda) 
+VALUES
+	(1, '2014-02-01', 4500)
+	,(1, '2014-02-03', 3400)
+	,(2, '2014-02-10', 2100)
+	,(3, '2014-02-15', 2700)
+	,(3, '2014-03-17', 560)
+	,(4, '2014-04-09', 1200)
+	,(5, '2014-05-07', 3500)
+	,(1, '2014-05-07', 3400)
+	,(2, '2014-05-05', 4000);
 
-INSERT INTO tbItensVenda (codVenda, codProduto, quantidadeItensVenda, subTotalItensVenda)
+INSERT INTO tbItensVenda(codVenda, codProduto, quantidadeItensVenda, subTotalItensVenda) 
 VALUES
 	(1, 1, 200, 1500)
 	,(1, 2, 300, 3000)
@@ -60,10 +60,8 @@ VALUES
 	,(5, 5, 200, 560)
 	,(6, 7, 200, 600)
 	,(6, 6, 300, 600)
-	,(8, 1, 300, 2500)
-	,(8, 2, 200, 1000)
-	,(9, 6, 250, 1700)
-	,(9, 5, 200, 1700)
-	,(10, 4, 1000, 4000);
-
-SELECT * FROM tbItensVenda
+	,(7, 1, 300, 2500)
+	,(7, 2, 200, 1000)
+	,(8, 6, 250, 1700)
+	,(8, 5, 200, 1700)
+	,(9, 4, 1000, 4000);
