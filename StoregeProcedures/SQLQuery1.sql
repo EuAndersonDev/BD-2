457ab6c7665ac0147fc33f95eed53040c42b5b31
ANDERSON REIS <137122362+EuAndersonDev@users.noreply.github.com>
2024-08-26 19:58:05 -0300

    Add files via upload

---
+++ StoregeProcedures/SQLQuery1.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE db_EscolaIdiomas
+USE db_EscolaIdiomas
+GO
+
+CREATE TABLE tbl_aluno(
+    codAluno INT PRIMARY KEY IDENTITY(1,1),
+    nomeAluno VARCHAR(250) NOT NULL,
+    dataNascAluno DATE NOT NULL,
+    rgAluno CHAR(9) NOT NULL,
+    naturalidadeAluno CHAR(2)
+)
+ 
+CREATE TABLE tbl_curso(
+    codCurso INT PRIMARY KEY IDENTITY(1,1),
+    nomeCurso VARCHAR(10) NOT NULL,
+    cargaHorariaCurso INT NOT NULL,
+    valorCurso MONEY NOT NULL
+)
+ 
+CREATE TABLE tbl_turma(
+    codTurma INT PRIMARY KEY IDENTITY(1,1),
+    nomeTurma VARCHAR (10) NOT NULL,
+    horarioTurma TIME NOT NULL,
+    codCurso INT FOREIGN KEY REFERENCES tbl_curso(codCurso)
+)
+ 
+CREATE TABLE tbl_matricula(
+    codMatricula INT PRIMARY KEY IDENTITY(1,1),
+    dataMatricula DATE NOT NULL,
+    codAluno INT FOREIGN KEY REFERENCES tbl_aluno(codAluno),
+    codTurma INT FOREIGN KEY REFERENCES tbl_turma(codTurma)
+)
+ 
+INSERT INTO tbl_aluno (nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)VALUES
+('Paulo Santos', '2000/03/10', '822821220', 'SP'),
+('Maria da Gloria', '1999/10/03', '452331230', 'SP'),
+('Pedro Nogueira da silva', '1998/04/04', '235332119', 'SP'),
+('Gilson Barros Silva', '1995/09/09', '34221111x', 'PE'),
+('Mariana Barbosa Santos', '2001/10/10', '542221229', 'RJ'),
+('Alessandro Pereira', '2003/10/11', '244022549', 'ES'),
+('Aline Melo', '2001/10/08', '883650123', 'RJ')
+ 
+INSERT INTO tbl_curso(nomeCurso, cargaHorariaCurso, valorCurso)VALUES
+('Inglês', '2000', '356,00'),
+('Alemão', '3000', '478,00'),
+('Espanhol', '4000', '500,00')
+ 
+INSERT INTO tbl_turma(nomeTurma, codCurso, horarioTurma)VALUES
+('1IA', '1', '12:00:00'),
+('1EC', '3', '18:00:00'),
+('1IB', '1', '18:00:00'),
+('1AA', '2', '19:00:00')
+ 
+INSERT INTO tbl_matricula(dataMatricula, codAluno, codTurma)VALUES
+('2015/10/03', '1', '1'),
+('2014/05/04', '2', '1'),
+('2015/05/04', '2', '4'),
+('2012/05/03', '3', '1'),
+('2016/03/03', '1', '3'),
+('2015/05/07', '4', '2'),
+('2015/07/05', '4', '3')
---
+++ StoregeProcedures/SQLQuery2.sql
@@ -0,0 +1,138 @@
+USE db_EscolaIdiomas
+
+/*EX1*/
+CREATE PROCEDURE spBuscaAluno
+    @codAluno INT
+AS
+BEGIN
+    SELECT nomeAluno, dataNascAluno
+    FROM tbl_aluno
+    WHERE codAluno = @codAluno;
+END;
+
+DROP PROCEDURE spBuscaAluno
+
+EXEC spBuscaAluno @codAluno = 3;
+
+/*EX2*/
+CREATE PROCEDURE spInserirAluno
+    @nomeAluno VARCHAR(250),
+    @dataNascAluno DATE,
+    @rgAluno CHAR(9),
+    @naturalidadeAluno CHAR(2)
+AS
+BEGIN
+	INSERT INTO tbl_aluno(nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)
+	VALUES (@nomeAluno, @dataNascAluno, @rgAluno, @naturalidadeAluno)
+END;
+
+EXEC spInserirAluno 
+    @nomeAluno = 'Carlos Silva', 
+    @dataNascAluno = '2002-05-15', 
+    @rgAluno = '123456789', 
+    @naturalidadeAluno = 'RJ';
+
+SELECT * FROM tbl_aluno;
+
+/*EX3*/
+CREATE PROCEDURE spAumentaPrecoCurso
+    @nomeCurso VARCHAR(10),
+    @percentualAumento DECIMAL(5, 2)
+AS
+BEGIN
+    UPDATE tbl_curso
+    SET valorCurso = valorCurso * (1 + @percentualAumento / 100)
+    WHERE nomeCurso = @nomeCurso;
+
+    SELECT valorCurso AS NovoValorCurso
+    FROM tbl_curso
+    WHERE nomeCurso = @nomeCurso;
+END;
+
+EXEC spAumentaPrecoCurso 
+    @nomeCurso = 'Inglês', 
+    @percentualAumento = 10.0;
+
+/*EX4*/
+CREATE PROCEDURE spExibeTurma
+    @nomeTurma VARCHAR (10)	
+AS
+BEGIN
+    SELECT codTurma, nomeTurma, horarioTurma, codCurso
+    FROM tbl_turma
+    WHERE nomeTurma = @nomeTurma;
+END;
+
+EXEC spExibeTurma @nomeTurma = "1IA";
+
+/*5*/
+CREATE PROCEDURE spExibeAlunosPorTurma
+    @nomeTurma VARCHAR(10)
+AS
+BEGIN
+    SELECT a.codAluno, a.nomeAluno, a.dataNascAluno
+    FROM tbl_aluno a
+    INNER JOIN tbl_matricula m ON a.codAluno = m.codAluno
+    INNER JOIN tbl_turma t ON m.codTurma = t.codTurma
+    WHERE t.nomeTurma = @nomeTurma;
+END;
+
+EXEC spExibeAlunosPorTurma @nomeTurma = '1IA';
+
+/*6*/
+CREATE PROCEDURE spInsereAlunoComVerificacao
+    @nomeAluno VARCHAR(250),
+    @dataNascAluno DATE,
+    @rgAluno CHAR(9),
+    @naturalidadeAluno CHAR(2)
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM tbl_aluno WHERE rgAluno = @rgAluno)
+    BEGIN
+        PRINT 'Aluno com esse RG já existe.';
+    END
+    ELSE
+    BEGIN
+        -- Insere o novo aluno, pois o RG não foi encontrado
+        INSERT INTO tbl_aluno (nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)
+        VALUES (@nomeAluno, @dataNascAluno, @rgAluno, @naturalidadeAluno);
+        PRINT 'Aluno inserido com sucesso.';
+    END
+END;
+
+EXEC spInsereAlunoComVerificacao 
+    @nomeAluno = 'Carlos Silva', 
+    @dataNascAluno = '2002-05-15', 
+    @rgAluno = '123456789', 
+    @naturalidadeAluno = 'RJ';
+
+
+
+/*7*/
+CREATE PROCEDURE spMatriculaAlunoNoCurso
+    @nomeCurso VARCHAR(10),
+    @nomeAluno VARCHAR(250)
+AS
+BEGIN
+    DECLARE @codCurso INT;
+    DECLARE @codAluno INT;
+    DECLARE @codTurma INT;
+    
+    SELECT @codCurso = codCurso
+    FROM tbl_curso
+    WHERE nomeCurso = @nomeCurso;
+
+    SELECT @codAluno = codAluno
+    FROM tbl_aluno
+    WHERE nomeAluno = @nomeAluno;
+
+    INSERT INTO tbl_matricula (dataMatricula, codAluno, codTurma)
+    VALUES (GETDATE(), @codAluno, @codTurma);
+
+    PRINT 'Aluno matriculado com sucesso.';
+END;
+GO
+
+EXEC spMatriculaAlunoNoCurso 
+    @nomeCurso = 'Inglês', 
+    @nomeAluno = 'Carlos Silva';
